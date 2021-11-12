5cda5dc31a7fde552f0baff9f861e1b62ecfba87
Ryan DeVore <ryan@silverpine.com>
2021-11-12 07:53:13 -0800

    Add Intent extensions

---
+++ ux/src/main/java/com/silverpine/uu/ux/IntentExtensions.kt
@@ -0,0 +1,17 @@
+package com.silverpine.uu.ux
+
+import android.content.Intent
+import android.os.Parcelable
+
+fun <T: Parcelable> Intent.uuRequireParcelable(key: String): T
+{
+    val obj = extras?.getParcelable<T>(key)
+    if (obj == null)
+    {
+        throw RuntimeException("Expected extra with key $key to be non-nil.")
+    }
+    else
+    {
+        return obj
+    }
+}
